d16adf0e6db215bcb4ec06ca9b7fb588a31a260c
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2026-06-22 20:06:08 +0200

    template strings and concatenation

---
+++ html/script/concatenate.js
@@ -0,0 +1,9 @@
+// concatenate
+let name = "John";
+let surname = "Doe";
+ let fullName1 = name + " " + surname;
+console.log(fullName1);
+console.log(name, surname);
+//Template string
+let fullName =`${name} ${surname}`;
+console.log(fullName);
